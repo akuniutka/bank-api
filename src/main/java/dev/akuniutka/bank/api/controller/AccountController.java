a3713c9f40f3fa1d25a5f84abd58a72376f30381
Andrei Kuniutka <akuniutka@gmail.com>
2023-08-10 22:30:41 +0300

    downgraded Spring Boot version to 2.5.14 for compatibility with Swagger, enabled Swagger

--- src/main/java/dev/akuniutka/bank/api/controller/AccountController.java
+++ src/main/java/dev/akuniutka/bank/api/controller/AccountController.java
@@ -3,7 +3,7 @@ package dev.akuniutka.bank.api.controller;
 import dev.akuniutka.bank.api.dto.CashOrderDto;
 import dev.akuniutka.bank.api.dto.ResponseDto;
 import dev.akuniutka.bank.api.service.AccountService;
-//import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -17,13 +17,13 @@ public class AccountController {
     }
 
     @GetMapping("/getBalance/{userId}")
-//    @Operation(summary = "Get the current balance for a selected user")
+    @Operation(summary = "Get the current balance for a selected user")
     public ResponseDto getBalance(@PathVariable Long userId) {
         return new ResponseDto(service.getUserBalance(userId));
     }
 
     @PutMapping("/putMoney")
-//    @Operation(summary = "Put money to user's account")
+    @Operation(summary = "Put money to user's account")
     public ResponseDto putMoney(@RequestBody CashOrderDto order) {
         service.increaseUserBalance(order.getUserId(), order.getAmount());
         return new ResponseDto(BigDecimal.ONE);
