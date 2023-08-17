7d79b7297a9250c20fac6e8fcffe43afc4ed8ce2
Andrei Kuniutka <akuniutka@gmail.com>
2023-08-18 00:15:24 +0300

    added to interface OperationRepository method findByAccountAndDateBetween

--- src/main/java/dev/akuniutka/bank/api/repository/OperationRepository.java
+++ src/main/java/dev/akuniutka/bank/api/repository/OperationRepository.java
@@ -1,7 +1,12 @@
 package dev.akuniutka.bank.api.repository;
 
+import dev.akuniutka.bank.api.entity.Account;
 import dev.akuniutka.bank.api.entity.Operation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OperationRepository extends CrudRepository<Operation, Long> {
+    List<Operation> findByAccountAndDateBetween(Account account, Date start, Date finish);
 }
